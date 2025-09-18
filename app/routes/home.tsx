65c4dcd2d6a1c9e346d8fa55f5adb5c2565928da
ADITYA-CD <aditya3102004@gmail.com>
2025-09-18 21:55:55 +0530

    components

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,32 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/resumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ClearPath" },
+    { name: "description", content: "Simplifying resumes with AI" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page_heading py-16">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Your AI-powered resume builder</h2>
+      </div>
+
+      {resumes.length > 0 && (
+        <div className="resume-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </section>
+  </main>;
 }
